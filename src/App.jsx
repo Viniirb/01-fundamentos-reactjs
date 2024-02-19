9cc7e6a72d51cff24dc6244e42c0fd94d81c536a
Vinicius Rolim Barbosa <vinicius.barbosa@eossystems.com.br>
2024-02-19 14:31:57 -0400

    - Styling the Header component with the Ignite Logo. - Importing Google fonts. - Standard styling of colors, fonts, and system styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
+import './global.css'
+
 
 function App() {
 
   return (
     <div>
+      <Header />
+
       <Post 
       author= "Vinicius Rolim Barbosa" 
       content= "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis hic nulla blanditiis ad laudantium quod officiis doloremque non consequatur, eos voluptate iusto facere unde fuga velit saepe quia dolor ea."
