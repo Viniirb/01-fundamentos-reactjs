a62c3c4c3fb6093a032c38192603381dcb9ebc64
Vinicius Rolim Barbosa <vinicius.barbosa@eossystems.com.br>
2024-02-19 15:54:59 -0400

    - Finished styling the Sidebar component. - Imported from the phospor-icons icon library. - Styling the Sidebar component properties.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import {PencilLine} from 'phosphor-react';
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -8,12 +9,16 @@ export function Sidebar(){
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Viniirb.png"/>
+
+
                 <strong>Vinicius Rolim Barbosa</strong>
                 <span>FullStack Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
