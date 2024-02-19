2cc1d5ffd8869d8f2f1317c7e12e30b10d1ff603
Vinicius Rolim Barbosa <vinicius.barbosa@eossystems.com.br>
2024-02-19 15:26:45 -0400

    - Initializing sidebar component. - Starting component styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 
 function App() {
@@ -9,17 +11,20 @@ function App() {
     <div>
       <Header />
 
-      <Post 
-      author= "Vinicius Rolim Barbosa" 
-      content= "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis hic nulla blanditiis ad laudantium quod officiis doloremque non consequatur, eos voluptate iusto facere unde fuga velit saepe quia dolor ea."
-    />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author= "Vinicius Rolim Barbosa" 
+            content= "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis hic nulla blanditiis ad laudantium quod officiis doloremque non consequatur, eos voluptate iusto facere unde fuga velit saepe quia dolor ea."
+          />
     
-    <Post 
-      author= "Teste" 
-      content= "Um novo posto muito legal!"
-    />
-    
-
+          <Post 
+            author= "Teste" 
+            content= "Um novo posto muito legal!"
+          />
+        </main>
+      </div>  
     </div>    
   )
 }
