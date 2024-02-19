a523542ef1bf6cf8b902b4a1a7d17a54f4120b71
Vinicius Rolim Barbosa <vinicius.barbosa@eossystems.com.br>
2024-02-19 13:35:04 -0400

    Studying content about react component properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import { Post } from "./Post"
 function App() {
 
   return (
-    <Post />
+    <div>
+      <Post 
+      author= "Vinicius Rolim Barbosa" 
+      content= "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis hic nulla blanditiis ad laudantium quod officiis doloremque non consequatur, eos voluptate iusto facere unde fuga velit saepe quia dolor ea."
+    />
+    
+    <Post 
+      author= "Teste" 
+      content= "Um novo posto muito legal!"
+    />
+    
+
+    </div>    
   )
 }
 
