91a2ef457dfe0aca971c36d3c92fec96b6fcf43f
Vinicius Rolim Barbosa <vinicius.barbosa@eossystems.com.br>
2024-02-29 15:26:39 -0400

    - Implementation of the comment component. - Styling the comments list

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/Viniirb.png" alt="Avatar" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Vinicius</strong>
+                            <time title="26 de fevereiro as 15:09" dateTime="2024-02-26">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom, parabéns!!</p>
+                </div>
+                
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
